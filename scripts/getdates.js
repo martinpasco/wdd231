7d4cbb48b5c42982dc6da4415fb5616bb83816b7
martinpasco <martinpasco@hotmail.com>
2025-09-06 22:12:20 -0400

    footer
    update

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+
+
+//value de year
+const date = new Date();
+const year = date.getFullYear();
+document.getElementById('curYr').innerHTML = year;
+
+const text = document.lastModified;
+document.getElementById("demo").innerHTML = text;
